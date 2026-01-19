af03b8555b0577b00dc921045ec7725f5e48370b
Nikke <nikkemarttila@gmail.com>
2026-01-19 12:53:36 +0200

    Fixes

--- mod-02/2-6.py
+++ mod-02/2-6.py
@@ -1,19 +1,21 @@
 import random
 
-# Kolmenumeroinen koodi (0–9)
-threeDigit = (
-    random.randint(0, 9),
-    random.randint(0, 9),
-    random.randint(0, 9)
-)
+# Kolmenumeroisen koodin generointi (0–9)
+threeOne = str(random.randint(0, 9))
+threeTwo = str(random.randint(0, 9))
+threeThree = str(random.randint(0, 9))
 
-# Nelinumeroinen koodi (1–6)
-fourDigit = (
-    random.randint(1, 6),
-    random.randint(1, 6),
-    random.randint(1, 6),
-    random.randint(1, 6)
-)
+# Catenoidaan kolme yksittäistä numeroa.
+threeDigit = threeOne + threeTwo + threeThree
+
+# Nelinumeroisen koodin generointi (1–6)
+fourOne = str(random.randint(1, 6))
+fourTwo = str(random.randint(1, 6))
+fourThree = str(random.randint(1, 6))
+fourFour = str(random.randint(1, 6))
+
+# Catenoidaan neljä yksittäistä numeroa.
+fourDigit = fourOne + fourTwo + fourThree + fourFour
 
 print("Kolmenumeroinen koodi:", threeDigit)
 print("Nelinumeroinen koodi:", fourDigit)
