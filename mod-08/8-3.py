b7178ccbbb895ef8797dfdf421f515d0cf6b9bb3
Nikke <nikkemarttila@gmail.com>
2026-02-09 08:09:46 +0200

    8. moduuli

--- mod-08/8-3.py
+++ mod-08/8-3.py
@@ -6,4 +6,52 @@ Laske etäisyys geopy-kirjaston avulla: https://geopy.readthedocs.io/en/stable/.
 Asenna kirjasto valitsemalla View / Tool Windows / Python Packages. Kirjoita hakukenttään geopy ja vie asennus loppuun.
 
 """
+import mysql.connector
+from geopy import distance
+
+# Tietokannan yhteys
+db = mysql.connector.connect(
+    host="127.0.0.1",
+    port="3306",
+    user="admin",
+    password ="admin",
+    database="flight_game"
+)
+
+def calculateDistance(icao1, icao2):
+
+    # Kysely: valitse ICAO vastaavuudets
+    query = f"SELECT latitude_deg, longitude_deg FROM airport WHERE iata_code = '{icao1}' OR iata_code = '{icao2}'"
+    # MUodosta yhteys tietokantaan
+    cursor = db.cursor()
+    # Suorita kysely
+    cursor.execute(query)
+    # Nouda tulokset
+    result = cursor.fetchall()
+
+    # Muuttujat
+    # AirportData = sanakirja joka säilöö koordinaatit
+    airportData = {}
+    # indeksi jota käytetään for loopissa
+    i = 1
+
+    for row in result:
+        print(row[0], row[1])
+        airportData[i] = row[0], row[1]
+        i += 1
+
+    # debug viesti
+    print("DEBUG: Sanakirjaan tallennettu: ", airportData)
+
+    # Geopyn distance-funktio mille annetaan sanakirjan koordinaatit parametreinä
+    geopyDistance = distance.distance(airportData[1], airportData[2])
+    # Tulosta koordinaattien välinen etäisyys
+    print(geopyDistance)
+
+
+
+
+### Pääohjelma
+# Kutsu funktiota lentokenttien ICAO koodeilla.
+calculateDistance("HEL", "BRU")
 
